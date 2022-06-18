7c373d0747f2188c1978ffb5925c12a198e318f6
yuken <yuken0906dev@gmail.com>
2022-06-18 19:20:17 +0900

    implement tooltip

--- components/BlogFooter.tsx
+++ components/BlogFooter.tsx
@@ -4,17 +4,33 @@ import { Tag } from './Tag';
 import Twitter from '../images/Twitter.svg'
 import Clip from '../images/Clip.svg'
 
-import {
-  TwitterShareButton,
-} from 'react-share';
+import { TwitterShareButton } from 'react-share';
+import { useState } from "react";
 
 type BlogFooterProps = {
   tags: ITag[]
   blog: IBlog
 }
 
+type CopiedTooltipPropsType = {
+  isHidden: boolean
+}
+
+const CopiedTooltip = (props: CopiedTooltipPropsType) => {
+  const { isHidden } = props;
+  return (
+    <div
+    className="absolute -top-8 p-1 rounded text-slate-400"
+    hidden={isHidden}
+    >
+      copied!
+    </div>
+  )
+}
+
 export const BlogFooter = (props: BlogFooterProps): JSX.Element => {
   const { tags, blog } = props;
+  const [isHiddenCopied, setIsHiddenCopied ] = useState(true);
 
   const getURL = (blogId: string): string => {
     return `https://www.yukendev.com/blog/${blogId}aaa`
@@ -23,6 +39,10 @@ export const BlogFooter = (props: BlogFooterProps): JSX.Element => {
   const copyButtonHandler = () => {
     try {
       navigator.clipboard.writeText(location.href)
+      setIsHiddenCopied(false);
+      setTimeout(() => {
+        setIsHiddenCopied(true);
+      }, 2000)
     }
     catch {
       console.log('An Error was occured with copy')
@@ -51,9 +71,12 @@ export const BlogFooter = (props: BlogFooterProps): JSX.Element => {
         </TwitterShareButton>
 
         {/* コピー */}
-        <button className="ml-3" onClick={copyButtonHandler}>
-          <Clip widht={40} height={40} />
-        </button>
+        <div className="relative flex justify-center ml-3">
+          <CopiedTooltip isHidden={isHiddenCopied}/>
+          <button onClick={copyButtonHandler}>
+            <Clip widht={40} height={40} />
+          </button>
+        </div>
 
       </div>
 
