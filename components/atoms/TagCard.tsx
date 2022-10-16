258880d4fbb91cf5ace7d5948815ec774f2ac472
yuken <yuken0906dev@gmail.com>
2022-10-16 11:50:33 +0900

    implement home design

--- components/atoms/TagCard.tsx
+++ components/atoms/TagCard.tsx
@@ -5,11 +5,14 @@ type TagProps = {
   tag: Tag;
 };
 
+const textStyle = 'text-myTextColor text-sm'
+const opacityStyle = 'opacity-70 hover:opacity-50'
+
 export const TagCard = (props: TagProps): JSX.Element => {
   const { tag } = props;
   return (
     <Link href={`/tags/${tag}`}>
-      <div className='px-3 bg-myTagColor cursor-pointer text-white rounded-lg mr-1 text-sm my-1'>
+      <div className={`px-3 bg-myTagColor cursor-pointer font-bold rounded-lg mr-1  my-1 ${textStyle} ${opacityStyle}`}>
         {tag}
       </div>
     </Link>
