6c872ccb1a49db4811b8f50baf394dec27312bfe
yuken <yuken0906dev@gmail.com>
2022-10-16 14:56:15 +0900

    implement about page design

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -13,17 +13,17 @@ const AboutPage = (): JSX.Element => {
         <div className='flex justify-center my-5'>
           <Image
             className='rounded-3xl'
-            src='/icon.jpg'
-            width={150}
-            height={150}
+            src='/about.png'
+            width={200}
+            height={200}
             alt='My avatar'
           />
         </div>
 
         {/* 名前 */}
         <div className='flex flex-col content-center my-2'>
-          <div className='text-xl font-medium text-center'>てずか</div>
-          <div className='text-center'>@tezuka_0906_wtf</div>
+          <div className='text-xl font-medium text-center font-medium'>てずか</div>
+          <div className='text-center font-medium'>@tezuka_0906_wtf</div>
         </div>
 
         {/* 各種アカウント */}
@@ -40,15 +40,16 @@ const AboutPage = (): JSX.Element => {
         </div>
 
         {/* About Me */}
-        <p className='mx-auto text-xl mt-10'>About Me</p>
+        <p className='mx-auto text-xl mt-10 font-medium'>About Me</p>
         <div className='p-3'>
-          <div className='bg-myBlogContentBgColor rounded-3xl p-4 md:p-8 mx-auto max-w-xl'>
-            <p className='font-medium'>
+          <div className='mx-auto max-w-xl'>
+            <p className='font-medium leading-relaxed'>
               現在、大阪大学を休学して東京の会社でインターンをしながら勉強しています。
-              『休学中にプログラミング飽きたらどうしよう🥺』とか思ってた時期もありましたが、余裕でまだおもろいです。
+              『休学中にプログラミング飽きたらどうしよう🥺』とか思ってた時期もありましたが、まだおもろいです。
               少し前までSwiftでiOSアプリの開発をしていて、現在はNext.js,
               React,TypeScriptを勉強しながら
-              『GROWI』というOSSのwikiシステムの開発に携わっています。みなさん、『GROWI』を使いましょう。
+              <a href='https://growi.org/' style={{ textDecoration: 'underline' }}>『GROWI』</a>
+              というOSSのwikiシステムの開発に携わっています。
               勉強時間的にはフロントの方が多いですが、バックエンド、インフラにも興味あります。
               体を構成している全要素は『Vtuber』『銭湯』『テニス』です。それ以外の要素は体にありません。
             </p>
@@ -56,9 +57,9 @@ const AboutPage = (): JSX.Element => {
         </div>
 
         {/* About Blog */}
-        <p className='mx-auto text-xl mt-10'>About Blog</p>
+        <p className='mx-auto text-xl mt-10 font-medium'>About Blog</p>
         <div className='p-3'>
-          <div className='bg-myBlogContentBgColor rounded-3xl p-4 md:p-8 mx-auto max-w-xl'>
+          <div className='mx-auto max-w-xl'>
             <p className='font-medium'>
               以前まで、
               <a className='underline' href='https://www.bouzuyahonpo.com/'>
