3a5fd63f34bcf9a24ac866aa26ef0e5f37679c0f
yukendev <yuken0906dev@gmail.com>
2023-04-06 02:33:24 +0000

    refactor tag in TagPage

--- components/organisms/TagContentWrapper.tsx
+++ components/organisms/TagContentWrapper.tsx
@@ -14,10 +14,10 @@ export const TagContentWrapper = (
   return (
     <div className='mx-auto w-11/12 md:w-8/12 max-w-5xl'>
       {/* タグタイトル */}
-      <div className='mt-10 text-2xl font-bold text-myTextColor'>{tag}</div>
+      <h1 className='mt-10 text-2xl font-bold text-myTextColor'>{tag}</h1>
 
       {/* divider */}
-      <div className='border-t-2 border-myBgColorDark my-5'></div>
+      <hr className='border-t-2 border-myBgColorDark my-5'></hr>
 
       {/* ブログリスト */}
       <BlogListWrapper blogs={blogs} />
