74237e6e1a597e6c28be2d1665e4d5a9488a8c61
yukendev <yuken0906dev@gmail.com>
2023-05-07 13:13:45 +0000

    refactor CategoryDescription

--- components/atoms/CategoryDescription.tsx
+++ components/atoms/CategoryDescription.tsx
@@ -1,4 +1,5 @@
 import type { Category } from '../../constants/categories';
+import { categoryDescription } from '../../constants/categoryDescription';
 
 type Props = {
   category: Category;
@@ -12,11 +13,11 @@ export const CategoryDescription = (
   const RenderDescription = () => {
     const desc = (): string => {
       if (category === 'tech') {
-        return '技術系で学んだことを備忘録的に書きます。';
+        return categoryDescription.tech;
       } else if (category === 'book') {
-        return '読んだ本の感想などを書きます。まだ方針は決まってないですが、技術書のことがメインになりそうです。';
+        return categoryDescription.book;
       } else if (category === 'note') {
-        return '『note』とかいうかっこいい括りにしてますが、『その他』です。技術系のことでも、本系のことでもないものを書きます。';
+        return categoryDescription.note;
       }
     };
     return <p className='text-myTextColor'>{desc()}</p>;
