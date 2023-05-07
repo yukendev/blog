06363ce4b043428676ee97be9d35d883824637b3
yukendev <yuken0906dev@gmail.com>
2023-05-07 13:17:40 +0000

    refactor atom

--- components/atoms/TagCard.tsx
+++ components/atoms/TagCard.tsx
@@ -1,19 +1,18 @@
 import { Tag } from '../../constants/tags';
 import Link from 'next/link';
 
-type TagProps = {
+type Props = {
   tag: Tag;
 };
 
-const textStyle = 'text-myTextColor text-sm'
 const opacityStyle = 'opacity-70 hover:opacity-50'
 
-export const TagCard = (props: TagProps): JSX.Element => {
+export const TagCard = (props: Props): JSX.Element => {
   const { tag } = props;
   return (
     <Link href={`/tags/${tag}`}>
       <li className={`my-1 mr-1 rounded-lg px-3 bg-myTagColor cursor-pointer ${opacityStyle}`}>
-        <a className={`font-bold ${textStyle}`}>
+        <a className='font-bold text-myTextColor text-sm'>
           {tag}
         </a>
       </li>
