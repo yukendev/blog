69fe46b7bf7bf632455ad79096a434eedf2bc5a6
yukendev <yuken0906dev@gmail.com>
2023-11-28 13:53:35 +0000

    chore: test api

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -5,25 +5,60 @@ const myId = process.env.LINE_API_MY_ID;
 const accessToken = process.env.LINE_API_ACCESS_TOKEN;
 
 const handler = (req, res) => {
-  const data = JSON.stringify({
-    to: myId,
-    messages: [
-      {
-        type: 'text',
-        text: 'hello',
+  let reqStr;
+  let resStr;
+
+  try {
+
+    reqStr = req.toString();
+    resStr = res.toString();
+
+    const data = JSON.stringify({
+      to: myId,
+      messages: [
+        {
+          type: 'text',
+          text: reqStr,
+        },
+        {
+          type: 'text',
+          text: resStr,
+        },
+      ],
+    });
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
       },
-    ],
-  });
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
-  const request = https.request(url, options);
-  request.write(data);
-  request.end();
+    };
+    const request = https.request(url, options);
+    request.write(data);
+    request.end();
+  }
+  catch (err) {
+    const data = JSON.stringify({
+      to: myId,
+      messages: [
+        {
+          type: 'text',
+          text: err.toString(),
+        },
+      ],
+    });
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    const request = https.request(url, options);
+    request.write(data);
+    request.end();
+  }
+  
 };
 
 export default handler;
