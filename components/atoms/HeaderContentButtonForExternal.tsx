63c19fcbd4e18783fe3f3941702a879cd0684f5b
yukendev <yuken0906dev@gmail.com>
2024-01-10 11:32:11 +0000

    style: refactor HeaderContentButtonForExternal with css modules

--- components/atoms/HeaderContentButtonForExternal.tsx
+++ components/atoms/HeaderContentButtonForExternal.tsx
@@ -1,13 +1,20 @@
-type Props = {
+import styles from './HeaderContentButtonForExternal.module.scss';
+
+type HeaderContentButtonForExternalProps = {
   title: string;
   href: string;
 };
 
 // 外部サイトを開くボタン
-export const HeaderContentButtonForExternal = (props: Props): JSX.Element => (
-  <div className='px-2 py-1 mx-3 text-myTextColor cursor-pointer deco-underline-text font-semibold'>
-    <a href={props.href} target='_blank' rel='noopener noreferrer'>
-      {props.title}
-    </a>
-  </div>
+export const HeaderContentButtonForExternal = (
+  props: HeaderContentButtonForExternalProps,
+): JSX.Element => (
+  <a
+    className={styles['header-content-button-for-external']}
+    href={props.href}
+    target='_blank'
+    rel='noopener noreferrer'
+  >
+    {props.title}
+  </a>
 );
