8d722b43e28aa4ba9a4caa07cc6e66f9437645e0
yukendev <yuken0906dev@gmail.com>
2024-01-10 16:00:56 +0000

    style: refactor TopPage with css modules

--- components/pages/TopPage.tsx
+++ components/pages/TopPage.tsx
@@ -1,6 +1,7 @@
 import { BlogListWrapper } from '@components/organisms/BlogListWrapper';
 import { CategoryList } from '@components/organisms/CategoryList';
 import { Blog } from 'types';
+import styles from './TopPage.module.scss';
 
 type TopPageProps = {
   blogs: Blog[];
@@ -8,9 +9,9 @@ type TopPageProps = {
 
 export const TopPage = ({ blogs }: TopPageProps) => {
   return (
-    <div className='mx-auto w-11/12 max-w-5xl'>
+    <div className={styles['top-page-wrapper']}>
       <CategoryList />
-      <h1 className='text-xl text-myTextColor font-bold my-10'>記事一覧</h1>
+      <h1>記事一覧</h1>
       <BlogListWrapper blogs={blogs} />
     </div>
   );
