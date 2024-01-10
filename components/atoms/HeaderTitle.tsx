8c294c7aa2baf2d26e15705cfc3cfeed5522a47e
yukendev <yuken0906dev@gmail.com>
2024-01-10 11:33:58 +0000

    style: refactor HeaderTitle with css modules

--- components/atoms/HeaderTitle.tsx
+++ components/atoms/HeaderTitle.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import TitleImg from '../../images/Title.svg';
+import styles from './HeaderTitle.module.scss';
 
 export const HeaderTitle = (): JSX.Element => (
   <Link href='/'>
-    <a className='cursor-pointer'>
+    <a className={styles['headet-title']}>
       <TitleImg width={200} />
     </a>
   </Link>
