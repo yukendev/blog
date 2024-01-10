2a57d90d7a3f0c80025f00986f0500f2b4ad9c6d
yukendev <yuken0906dev@gmail.com>
2024-01-10 15:37:34 +0000

    chore: remove unused link tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang='ja'>
-      <Head>
-        <link
-          href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.9.0/themes/prism-tomorrow.min.css'
-          rel='stylesheet'
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
