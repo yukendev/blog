6c5bd820149dddc574d5becf4a64232b3745da8c
yukendev <yuken0906dev@gmail.com>
2024-01-16 09:10:05 +0000

    refactor: blog page for app router

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import { BlogPage } from '@components/pages/Blog';
+import { getPostsBySlug, getSortedPostsData } from 'libs/posts';
+import { Metadata } from 'next/types';
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> => {
+  const blog = await getPostsBySlug(params.slug);
+
+  return {
+    title: blog?.title,
+    description: blog?.description,
+    openGraph: {
+      url: `https://www.yukendev.com/${blog?.slug}`,
+      title: blog?.title,
+      description: blog?.description,
+    },
+  };
+};
+
+export default function Page({ params }: { params: { slug: string } }) {
+  return <BlogPage slug={params.slug} />;
+}
+
+// 調べる
+export const generateStaticParams = async () => {
+  const data = await getSortedPostsData();
+
+  return data.map((post) => ({
+    slug: post.slug,
+  }));
+};
