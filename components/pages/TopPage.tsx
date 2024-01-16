b39c5f4fab63bdb51d800de80c61b59cc601da51
yukendev <yuken0906dev@gmail.com>
2024-01-16 09:09:25 +0000

    refactor: TopPage component for RSC

--- components/pages/TopPage.tsx
+++ components/pages/TopPage.tsx
@@ -1,13 +1,11 @@
 import { BlogListWrapper } from '@components/organisms/BlogListWrapper';
 import { CategoryList } from '@components/organisms/CategoryList';
-import { Blog } from 'types';
 import styles from './TopPage.module.scss';
+import { getSortedPostsData } from 'libs/posts';
 
-type TopPageProps = {
-  blogs: Blog[];
-};
+export const TopPage = async () => {
+  const blogs = await getSortedPostsData();
 
-export const TopPage = ({ blogs }: TopPageProps) => {
   return (
     <div className={styles['top-page-wrapper']}>
       <CategoryList />
